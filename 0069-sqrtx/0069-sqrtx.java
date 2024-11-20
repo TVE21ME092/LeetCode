638df96b9e2c533c0a9e5f8d409e940563fe4428
TVE21ME092 <navaneethjee@gmail.com>
2024-11-20 23:09:01 +0530

    Time: 1 ms (86.63%), Space: 41 MB (22.85%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,5 +1,18 @@
 class Solution {
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        int start=0;
+        int end=x;
+        int ans=0;
+        while(start<=end){
+            int mid=start-(start-end)/2;
+            if((long)mid*mid<=x){
+                ans=mid;
+                start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+        return ans;
     }
 }
